d319841ffb069f6b318d6aaaa71f647c4c0a945c
marylinkana <43268881+marylinkana@users.noreply.github.com>
2018-10-19 12:00:44 +0200

    config.php et connexio.php
    Modification

--- SiteParking3000/config.php
+++ SiteParking3000/config.php
@@ -1,3 +1,5 @@
 <?php
-global $bdd = new PDO ("mysql:host=localhost;dbname=parking3000","root","")
-    ?>
+    $bdduser = "root";
+    $bddmdp = "";
+    ?>  
+
--- SiteParking3000/connexion.php
+++ SiteParking3000/connexion.php
@@ -3,13 +3,18 @@ session_start();
 
 function bddConnection(){
     try
-    {
-        require "config.php" ;   
-    }
-    catch(Exeption $e)
-    {
-        die("bdd non trouvée");
-    }
+        {
+            require "config.php"
+            global $bdd = new PDO ("mysql:host=localhost;dbname=parking3000",$bdduser,$bddmdp);   
+        }
+        catch(Exeption $e)
+            {
+                die("bdd non trouvée");
+            }
+    
+    catch(Exception $e) { // S'il y a eu une erreur, alors...
+    echo 'Erreur : ' . $e->getMessage();
+}
 }
 
 include "index.php";
