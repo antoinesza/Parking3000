1fc70f20788241f95859701d2ad25204c6ee7f4d
antoine szajryck <antoine.szajryck@gmail.com>
2018-10-19 12:08:53 +0200

    inscription / login

--- SiteParking3000/controllers/insController.php
+++ SiteParking3000/controllers/insController.php
@@ -19,7 +19,7 @@ $email = htmlspecialchars(addslashes(trim($_POST['email'])));
 				if($password==$re_password)
 				{  
 						// Requete SQL pour savoir si un utilisateur posséde déja l'adresse email renseigner 
-						$sqlquery = "SELECT users_email FROM users WHERE users_email = '$email' ";
+						$sqlquery = "SELECT email FROM users WHERE email = '$email' ";
 						$sql = $bdd->query($sqlquery);
 						$sql = $sql->rowCount();
 						if($sql == 0) // Si aucune adresse email est trouvé dans la BDD
@@ -28,17 +28,17 @@ $email = htmlspecialchars(addslashes(trim($_POST['email'])));
 
 
 							// On insert dans la BDD une nouvelle ligne avec le mdp et l'email entrée par l'utilisateur                
-						    $sqlquery= "INSERT INTO users (users_email,users_password,users_premium_date) VALUES('$email','$password',null)";
+						    $sqlquery= "INSERT INTO users (email,password,date_inscription,date_naissance) VALUES('$email','$password',null)";
                            	$sql = $bdd->query($sqlquery);
 
 
 							// On récupére toutes les données sur l'utilisateurs qui vient de s'enregister
-							$sql_users = "SELECT * FROM users WHERE users_email = '$email'";
+							$sql_users = "SELECT * FROM users WHERE email = '$email'";
 							$sql_users = $bdd->query($sql_users);
 							$sql_users_data = $sql_users->fetch(PDO::FETCH_ASSOC);
 
 							// On créer un variable $lien en fonction de l'ID de l'utilisateur
-							$lien = "data/profils/" . $sql_users_data['users_id'] . ".jpg";
+							$lien = "" . $sql_users_data['id'] .;
 
 							
 							// On redirige l'utilisateur sur la page login.php
