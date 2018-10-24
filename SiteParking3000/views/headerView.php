c6280e67ab9a405ae718dfd42f8025adc081b7ad
antoine szajryck <antoine.szajryck@gmail.com>
2018-10-24 15:51:59 +0200

    réorganisation

--- SiteParking3000/views/headerView.php
+++ SiteParking3000/views/headerView.php
@@ -1,6 +1,6 @@
 
     <header>
-         <a href="accueil2View.php">Accueil</a>
+         <a href="accueilView.php">Accueil</a>
          <a href="loginView.php">Login</a> 
          <a href="insView.php">Inscription</a> 
          <a href="adminView.php">Admin</a> <br/>
