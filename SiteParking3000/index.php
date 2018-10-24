604496640431ec772427ef0cdd8da51438c4e5b0
antoine szajryck <antoine.szajryck@gmail.com>
2018-10-24 19:12:34 +0200

    Update SiteParking3000/index.php

--- SiteParking3000/index.php
+++ SiteParking3000/index.php
@@ -1,16 +1,24 @@
 <?php
 
-ob_start();//permet de suspendre l'affichage
+session_start();
+
+    try  {
+        $bdd = new PDO("mysql:host=localhost;dbname=parking3000;charset=uf8","root","");
+        }
+        catch(Exception $e)
+        {
+            die ("erreur de bdd non trouvée");
+        }
 
 
-        if(!isset($_GET['p']))
+if(!isset($_GET['p']) || $_GET['p'] == "")
         {
 
-            $page = "accueil;
+            $page = "accueil";
             
         }
 
-        else
+else
         {
             if(!file_exists("controllers/".$_GET['p']."Controller.php"))
                 $page = "404";
@@ -19,13 +27,10 @@ ob_start();//permet de suspendre l'affichage
                
         }
 
-
+        ob_start();
         include "controllers/".$page."Controller.php";
         $content = ob_get_contents();
-
-    
-
-ob_end_clean();
+        ob_end_clean();
 	
 	include "layout.php";
 ?>
